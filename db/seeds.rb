8a2026035669027599ac9685a689ea0b20787c45
Kenton Archer <kenton8654@yahoo.com>
2020-10-03 16:49:47 -0400

    seed data pt 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,25 @@
 
 
 
+
+
+
 Game.destroy_all
-Question.destroy_all 
+Question.destroy_all
+
+
+let picsArray = ["https://www.google.com/search?q=animals+in+the+tropics&tbm=isch&chips=q:animals+in+the+tropics,g_1:common:QhRWTm_A3HY%3D&hl=en&sa=X&ved=2ahUKEwjv2uaxkJnsAhVHZt8KHebbDH8Q4lYoAnoECAEQGA&biw=1440&bih=746#imgrc=lZelmtZU5y4AdM&imgdii=NOEbhva1F_LPIM",
+  "https://www.google.com/search?q=animals+in+the+tropics&tbm=isch&chips=q:animals+in+the+tropics,g_1:common:QhRWTm_A3HY%3D&hl=en&sa=X&ved=2ahUKEwjv2uaxkJnsAhVHZt8KHebbDH8Q4lYoAnoECAEQGA&biw=1440&bih=746#imgrc=p6f_9RiKlCnV8M",
+  "https://www.google.com/search?q=animals+in+the+tropics&tbm=isch&chips=q:animals+in+the+tropics,g_1:common:QhRWTm_A3HY%3D&hl=en&sa=X&ved=2ahUKEwjv2uaxkJnsAhVHZt8KHebbDH8Q4lYoAnoECAEQGA&biw=1440&bih=746#imgrc=6j1jOt8kXNoL-M",
+  "https://pixfeeds.com/images/animals/tigers/1280-497184774-roaring-tiger.jpg"
+  "https://p3w6d7a3.stackpathcdn.com/wp-content/uploads/2017/06/frog-red-eyed-tree-amphibian-tropical-rainforest.jpg",
+  "https://temperaterainforestess.weebly.com/uploads/4/1/9/4/41941047/header_images/1414819030.jpg",
+  "https://outdoornebraska.gov/wp-content/uploads/2018/10/gray-wolf-pixabay-e1539763148228.jpg",
+  "https://t2.ea.ltmcdn.com/en/images/9/5/3/img_what_is_the_habitat_of_the_white_tailed_deer_1359_paso_1_600.jpg",
+  "https://nas-national-prod.s3.amazonaws.com/styles/social_media_photo/s3/web_fb_web_a1_1902_16_barred-owl_sandra_rothenberg_kk.jpg",
+  
+
+
+
+
+ ]
